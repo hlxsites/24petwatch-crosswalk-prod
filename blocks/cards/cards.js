89b4efb059ddc69a0a18f0617dec9edaec4e0473
Ekrem Doğan <ekremney@users.noreply.github.com>
2023-07-25 17:34:52 +0200

    cards styling (#19)

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,5 +1,13 @@
 import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
 
+function wrapInAnchor(element, href) {
+  const anchor = document.createElement('a');
+  anchor.classList.add('wrapping-anchor');
+  anchor.href = href;
+  element.replaceWith(anchor);
+  anchor.appendChild(element);
+}
+
 export default function decorate(block) {
   /* change to ul, li */
   const ul = document.createElement('ul');
@@ -7,8 +15,16 @@ export default function decorate(block) {
     const li = document.createElement('li');
     li.innerHTML = row.innerHTML;
     [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
+      const href = li.querySelector('a')?.href;
+
+      if (div.children.length === 1 && div.querySelector('picture')) {
+        div.className = 'cards-card-image';
+        wrapInAnchor(div, href);
+      } else {
+        div.className = 'cards-card-body';
+        const h4 = li.querySelector('h4');
+        wrapInAnchor(h4, href);
+      }
     });
     ul.append(li);
   });
