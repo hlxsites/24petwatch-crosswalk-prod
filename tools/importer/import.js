85e4038affdb916b8c5210c220d3d614932e3c29
Felix Delval <fdelval@adobe.com>
2023-10-17 10:29:42 +0200

    What is in the repo

--- tools/importer/import.js
+++ tools/importer/import.js
@@ -46,46 +46,65 @@ const createMetadata = (main, document) => {
   return meta;
 };
 
-const createHero = (main, document) => {
-  const heroVideo = main.querySelector('herovideoplayer');
-  if (heroVideo) {
-    const title = heroVideo.getAttribute('title');
-    const description = heroVideo.getAttribute('description');
-    const percentage = heroVideo.getAttribute('percentage');
-    const ctaText = heroVideo.getAttribute('btntext');
-    const videoid = heroVideo.getAttribute('videoid');
-    const imgSrc = 'https://danaherls.scene7.com/is/image/danaher/hero-image?$danaher-transparent$';
-    const imgAlt = heroVideo.getAttribute('imagealt');
+const createHero = (main, document, newMain) => {
+  const hero = main.querySelector('div#mainContent > div > div:first-child');
+  if (hero) {
+    const titleNodes = hero.querySelector('h1').childNodes;
+
+    const h1 = document.createElement('h1');
+    for (let i = 0; i < titleNodes.length; i += 1) {
+      const node = titleNodes[i];
+      if (node.nodeType === 3) {
+        const text = document.createTextNode(node.textContent);
+        h1.append(text);
+      }
+      if (node.nodeType === 1) {
+        if (node.tagName === 'BR') {
+          h1.append(document.createElement('br'));
+        }
+
+        if (node.tagName === 'SPAN') {
+          const em = document.createElement('em');
+          em.textContent = node.textContent;
+          h1.append(em);
+        }
+      }
+    }
+
+    const description = hero.querySelector('h4').textContent;
+    const ctaText = hero.querySelector('a').textContent;
+    const ctaHref = hero.querySelector('a').getAttribute('href');
+    const imgSrc = hero.querySelector('img').getAttribute('src');
+    const imgAlt = hero.querySelector('img').getAttribute('alt');
 
     const img = document.createElement('img');
     img.setAttribute('src', imgSrc);
     img.setAttribute('alt', imgAlt);
 
     const div = document.createElement('div');
-    const h2 = document.createElement('h2');
-    h2.textContent = title;
-    div.append(h2);
+    div.append(h1);
     const p1 = document.createElement('p');
     p1.textContent = description;
     div.append(p1);
     const p2 = document.createElement('p');
+    const a = document.createElement('a');
     const strong = document.createElement('strong');
-    strong.textContent = percentage;
+    a.setAttribute('href', ctaHref);
+    a.textContent = ctaText;
+    strong.append(a);
     p2.append(strong);
     div.append(p2);
-    const p3 = document.createElement('p');
-    const videoElemHTML = `<a href="https://player.vimeo.com/video/${videoid}?loop=1&app_id=122963">${ctaText}</a>`;
-    p3.innerHTML = videoElemHTML;
-    div.append(p3);
 
     const cells = [
       ['Hero'],
-      [img, div],
+      [div, img],
     ];
 
     const block = WebImporter.DOMUtils.createTable(cells, document);
-    heroVideo.append(block);
+    newMain.append(block);
+    //hero.append(block);
   }
+
 };
 
 const createCards = (main, document) => {
@@ -382,8 +401,9 @@ export default {
   }) => {
     // define the main element: the one that will be transformed to Markdown
     const main = document.body;
+    const newMain = document.createElement('main');
     createFullLayoutSection(main, document);
-    createHero(main, document);
+    createHero(main, document, newMain);
     createCards(main, document);
     createLogoCloud(main, document);
     createWeSee(main, document);
@@ -397,15 +417,15 @@ export default {
     }
 
     // use helper method to remove header, footer, etc.
-    WebImporter.DOMUtils.remove(main, [
+    WebImporter.DOMUtils.remove(newMain, [
       'header',
       'footer',
       'component',
     ]);
 
     // create the metadata block and append it to the main element
-    createMetadata(main, document);
-    return main;
+    createMetadata(newMain, document);
+    return newMain;
   },
 
   /**
