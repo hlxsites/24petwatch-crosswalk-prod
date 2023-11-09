e9612f444d98fbe9cc3e5f634d3ed33b7027cd5f
Chris Bohnert <38424477+bohnertchris@users.noreply.github.com>
2023-11-09 09:45:09 +0100

    Carousel cleanup

--- tools/importer/transformers/carousel.js
+++ tools/importer/transformers/carousel.js
@@ -1,12 +1,17 @@
 function createCarousel(currentBlock, main, document) {
-    console.log('Carousel found!')
+
+    // Find carousel items and start array of items
     let carouselItems = currentBlock.querySelectorAll('div.cmp-carousel__item');
     let carousel = [['Carousel']];
 
+    // Iterate through items
     for (let i = 0; i < carouselItems.length; i += 1) {
+       
+        // Add divs for text and image elements
         const text = document.createElement('div');
         const image = document.createElement('div');
 
+        // Start array for single item to hold both text and image
         let carouselItem = [];
     
         // Text
@@ -24,13 +29,13 @@ function createCarousel(currentBlock, main, document) {
             image.append(img);
         }
 
-        carouselItem = [image, text];
+        // Concatenate text and image in array element and push to items
+        carouselItem = [text, image];
         carousel.push(carouselItem);
     
     }
-
-    console.log(carousel);
  
+    // Output carousel items array of arrays as a table
     const block = WebImporter.DOMUtils.createTable(carousel, document);
     main.append(block);
 
